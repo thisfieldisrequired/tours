734b9dc126339de35c71b653fe7a8627b70c9e7e
GG <sudodebug@gmail.com>
2023-02-18 12:54:19 +0500

    Add categories

--- vacancy/views.py
+++ vacancy/views.py
@@ -3,7 +3,15 @@ from django.views import View
 
 links = [{"title": "Мои приключения", "link": "adventures"}, {"title": "Мои фотографии", "link": "photos"},
          {"title": "Реклама у меня", "link": "promo"}, {"title": "Контакты", "link": "contacts"}, ]
+
+categories = [
+    {"id": "drinks", "title": "Напитки"},
+    {"id": "streetfood", "title": "Стритфуд"},
+    {"id": "lunches", "title": "Обеды"}
+]
+
+
 class VacancyView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, "vacancy/vacancy.html", {"links": links})
+        return render(request, "vacancy/vacancy.html", {"links": links, "categories": categories})
