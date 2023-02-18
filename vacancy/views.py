f20bb1337d01c6da6f3e838a89cc99408d9367a9
GG <sudodebug@gmail.com>
2023-02-18 12:43:09 +0500

    Add menu

--- vacancy/views.py
+++ vacancy/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
-
+links = [{"title": "Мои приключения", "link": "adventures"}, {"title": "Мои фотографии", "link": "photos"},
+         {"title": "Реклама у меня", "link": "promo"}, {"title": "Контакты", "link": "contacts"}, ]
 class VacancyView(View):
+
     def get(self, request, *args, **kwargs):
-        return render(request, "vacancy/vacancy.html", {})
+        return render(request, "vacancy/vacancy.html", {"links": links})
