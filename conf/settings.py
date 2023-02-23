212ca36608215cbd01e262c02a0f58a06cd33a86
GG <sudodebug@gmail.com>
2023-02-23 13:55:05 +0500

    Create contex_proccesor tours_data

--- conf/settings.py
+++ conf/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'tours.contex_processors.tours_data.tours_data',
             ],
         },
     },
